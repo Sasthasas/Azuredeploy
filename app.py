ccb8680341fd9fe8143fadfa320b1a7bf8bbc5ba
Sasthasas <sastha345@gmail.com>
2026-06-18 10:10:17 +0530

    Set up basic Flask application with home route

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello Sastha! Flask is running on Azure ✅"
+
+if __name__ == "__main__":
+    app.run(debug=True)
